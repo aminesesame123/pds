6c36dd17c481eec4bce38c160eb0fb33be97875a
aminesesame123 <167778805+aminesesame123@users.noreply.github.com>
2025-11-28 03:11:45 +1300

    first commit

---
+++ backend/Notification-Service/src/test/java/com/sesame/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesame.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
